0ccbcbc81a89a6cadbe315fa2fde6ce30392f56d
Mario Korzun <rkorzun2020@gmail.com>
2026-04-27 18:32:23 +0400

    Add iziToast styling for success and error messages; enhance snackbar functionality

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,40 @@
 import iziToast from 'izitoast';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = Number(event.currentTarget.elements.delay.value);
+  const state = event.currentTarget.elements.state.value;
+
+  createPromise(delay, state)
+    .then(delay => {
+      console.log(`✅ Fulfilled promise in ${delay}ms`);
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+        timeout: 100000,
+      });
+    })
+    .catch(delay => {
+      console.log(`❌ Rejected promise in ${delay}ms`);
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+  form.reset();
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}

7864f280fccf2d9b73423a6582babcace54e269b
Romario Korzun <60797864+mrkorzun@users.noreply.github.com>
2026-04-27 16:19:51 +0400

    Refactor timer functionality; add future date validation and update button state in 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,15 +1,70 @@
 import flatpickr from 'flatpickr';
 import iziToast from 'izitoast';
 
+const refs = {
+  btn: document.querySelector('button'),
+  input: document.querySelector('#datetime-picker'),
+
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+refs.btn.disabled = true;
+
+let userSelectedDate = 0;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    userSelectedDate = selectedDates[0];
+    const currentDate = Date.now();
+
+    if (userSelectedDate.getTime() < currentDate) {
+      refs.btn.disabled = true;
+      iziToast.error({
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+      });
+    } else {
+      refs.btn.disabled = false;
+    }
   },
 };
+
+flatpickr('#datetime-picker', options);
+
+function onBtnSetTimer() {
+  // console.log('Button click');
+  refs.btn.disabled = true;
+  refs.input.disabled = true;
+  return new Promise(resolve => {
+    const intervalID = setInterval(() => {
+      const now = Date.now();
+      const deltaTime = userSelectedDate.getTime() - now;
+      if (deltaTime <= 0) {
+        clearInterval(intervalID);
+        refs.input.disabled = false;
+        resolve('Time is up!');
+        return;
+      }
+      const currentTime = convertMs(deltaTime);
+
+      refs.days.textContent = addZero(currentTime.days);
+      refs.hours.textContent = addZero(currentTime.hours);
+      refs.minutes.textContent = addZero(currentTime.minutes);
+      refs.seconds.textContent = addZero(currentTime.seconds);
+    }, 1000);
+  });
+}
+
+function addZero(value) {
+  return String(value).padStart(2, '0');
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -29,3 +84,9 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
+
+refs.btn.addEventListener('click', () => {
+  onBtnSetTimer().then(message => {
+    console.log(message);
+  });
+});
